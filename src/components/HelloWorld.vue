6618576d5be5105035091e61837fec111e90f166
x.liu <1193287884@qq.com>
2021-06-21 10:51:35 +0800

    first commit create a simple form with no regular

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,142 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <form action="" class="col-md-4 col-md-offset-4 form-horizontal" >
+    <div class="form-group">
+      <label class="col-md-3 control-label" >单据编号: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >客户代码: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >销售订单号: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >客户订单号: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >客户物料号: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >订单类型: </label>
+      <select class="form-control">
+        <option value="0"></option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label class="col-md-3 control-label" >产品内控参数: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >产品要求代码: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >产品类型: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >型号: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >产品规格: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >是否特殊订单: </label>
+      <input type="radio" class="form-control" >
+    </div>
+    <div class="form-group">
+      <label class="col-md-3 control-label" >配比号: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >单位克重(g/cm2): </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >数量（卷）: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >原料重量/kg: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >压花类型: </label>
+      <select class="form-control">
+        <option value="0">无规压花</option>
+      </select>
+      <label class="col-md-3 control-label" >切边类型: </label>
+      <select class="form-control">
+        <option value="0"></option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label class="col-md-3 control-label" >宽幅: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >中心值: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >接头数: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >缺陷数: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >接头和缺陷总数: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >每个补充米数: </label>
+      <input type="text" class="form-control" >
+    </div>
+    <div class="form-group">
+      <label class="col-md-3 control-label" >接头缺陷标识: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >标签颜色: </label>
+      <input type="color" class="form-control" >
+      <label class="col-md-3 control-label" >标签模板: </label>
+      <select class="form-control">
+        <option value="0">常规标签</option>
+      </select>
+      <label class="col-md-3 control-label" >标签显示克重: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >产区: </label>
+      <select class="form-control">
+        <option value="0">临安公司</option>
+      </select>
+      <label class="col-md-3 control-label" >出口: </label>
+      <input type="radio" class="form-control" >
+    </div>
+    <div class="form-group">
+      <label class="col-md-3 control-label" >标签要求: </label>
+      <input type="text" class="form-control" >
+    </div>
+    <div class="form-group">
+      <label class="col-md-3 control-label" >收卷方向: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >打包方式: </label>
+      <input type="text" class="form-control" >
+    </div>
+    <div class="form-group">
+      <label class="col-md-3 control-label" >辅料: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >装车要求: </label>
+      <input type="text" class="form-control" >
+    </div>
+    <div class="form-group">
+      <label class="col-md-3 control-label" >单: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >托: </label>
+      <input type="text" class="form-control" >
+    </div>
+    <div class="form-group">
+      <label class="col-md-3 control-label" >销售备注: </label>
+      <input type="text" class="form-control" >
+    </div>
+    <div class="form-group">
+      <label class="col-md-3 control-label" >内控备注: </label>
+      <input type="text" class="form-control" >
+    </div>
+    <div class="form-group">
+      <label class="col-md-3 control-label" >审核意见: </label>
+      <input type="text" class="form-control" >
+    </div>
+    <div class="form-group">
+      <label class="col-md-3 control-label" >手册号: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >压花前规格: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >压花后规格: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >制单人: </label>
+      <select class="form-control">
+        <option value="0">超级用户</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label class="col-md-3 control-label" >批次代码: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >批次流水号: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >交货基地: </label>
+      <input type="text" class="form-control" >
+      <label class="col-md-3 control-label" >制单时间: </label>
+      <input type="date" class="form-control" >
+    </div>
+  </form>
 </template>
 
 <script>
